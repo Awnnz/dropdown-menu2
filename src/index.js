d25bfe2b5ba1db8ab1a8eba7998e7dcb7d9c0926
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-07-15 02:58:17 -0700

    finish dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 import './style.css';
 
-function createDropdown() {
-    let dropdownMenu = document.createElement('div');
+function createDropdown(options) {
+    const dropdownMenu = document.createElement('div');
     dropdownMenu.className = 'dropdown-menu';
     dropdownMenu.innerHTML = `
     <button>Select Options</button>
     <ul class='dropdown-list'>
-        <li>Option 1</li>
-        <li>Option 2</li>
-        <li>Option 3</li>
-        <li>Option 4</li>
-        <li>Option 5</li>
-        <li>Option 6</li>
     </ul>
     `;
+
+    const [selectBtn, dropdownList] = dropdownMenu.children;
+    selectBtn.className = 'select-button';
+
+    function toggleActiveClasses() {
+        selectBtn.classList.toggle('active');
+        dropdownList.classList.toggle('active');
+    };
+
+    options.forEach((option, index) => {
+        const optionElement = document.createElement('li');
+        optionElement.innerHTML = option;
+        optionElement.setAttribute('value', `Option ${index}`);
+
+        optionElement.addEventListener('click', (e) => {
+            selectBtn.textContent = e.target.getAttribute('value');
+            toggleActiveClasses();
+        });
+
+        dropdownList.appendChild(optionElement);
+    })
+    
+    selectBtn.addEventListener('click', () => {
+        toggleActiveClasses();
+    })
+
+    document.onclick = (e) => {
+        if (!dropdownMenu.contains(e.target)) {
+            selectBtn.classList.remove('active');
+            dropdownList.classList.remove('active');
+        };
+    };
 
     return dropdownMenu;
 }
 
-document.body.appendChild(createDropdown());
+document.body.appendChild(createDropdown(['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5']));
